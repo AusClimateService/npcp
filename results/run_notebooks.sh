cdf61338d01e753d0b6d7a225d0df286d5cdbd6a
Damien Irving <irving.damien@gmail.com>
2024-10-30 17:26:19 +1100

    Get notebooks running

--- results/run_notebooks.sh
+++ results/run_notebooks.sh
@@ -7,6 +7,16 @@
 
 notebooks=($(ls *.ipynb))
 for notebook in "${notebooks[@]}"; do
+#  sed -i -e 's/save_outfile=True/save_outfile=False/g' ${notebook}
+#  sed -i -e 's/sys.path.append/#sys.path.append/g' ${notebook}
+#  sed -i -e 's/Python \[conda env:analysis3-23.04\]/Python 3 (ipykernel)/g' ${notebook}
+#  sed -i -e 's/Python \[conda env:analysis3-24.04\]/Python 3 (ipykernel)/g' ${notebook}
+#  sed -i -e 's/Python \[conda env:analysis3-24.07\]/Python 3 (ipykernel)/g' ${notebook}
+#  sed -i -e 's/Python \[conda env:analysis3\]/Python 3 (ipykernel)/g' ${notebook}
+#  sed -i -e 's/conda-env-analysis3-23.04-py/python3/g' ${notebook}
+#  sed -i -e 's/conda-env-analysis3-24.04-py/python3/g' ${notebook}
+#  sed -i -e 's/conda-env-analysis3-24.07-py/python3/g' ${notebook}
+#  sed -i -e 's/conda-env-analysis3-py/python3/g' ${notebook}
   qsub -v notebook=${notebook} notebook_job.sh
 done
 
